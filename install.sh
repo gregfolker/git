ea9c37d0341ad469c567ab9e73ec1cda81aeabb2
gregfolker <greg.folker@pm.me>
2021-05-08 17:11:21 -0500

    feat: initial commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+INSTALL_SCRIPT="${0}"
+INSTALL_DIR="${0%/*}"
+
+GITCONFIG="$HOME/.gitconfig"
+
+echo "Running ${INSTALL_SCRIPT}..."
+
+if [[ -e ${GITCONFIG} ]] ; then
+   echo "Creating a backup of ${GITCONFIG} as ${GITCONFIG}-old..."
+   mv ${VIMRC} ${VIMRC}-old
+fi
+
+touch ${GITCONFIG} && ln -sf "${PWD}/.gitconfig" ${GITCONFIG} && echo "Successfully installed global git configuration"
